a154c31a30a74728c7387e4b4a4da73309d39a82
Felipe F. Schwade <felipefaeschwade@hotmail.com>
2017-03-07 09:44:35 -0400

    Todas as Atividades Até a Aula 11

---
+++ CaelumEstoque/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using CaelumEstoque.DAO;
+using CaelumEstoque.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CaelumEstoque.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Autentica(Usuario usuario)
+        {
+            var dao = new UsuariosDAO();
+            usuario = dao.Busca(usuario.Nome, usuario.Senha);
+            if (usuario != null)
+            {
+                Session["usuarioLogado"] = usuario;
+                return RedirectToAction("Index", "Produtos");
+            }
+            ModelState.AddModelError("usuario.Invalido", "Usuário ou senha inválidos");
+            return View("Index");
+        }
+    }
+}
---
+++ CaelumEstoque/Models/Produto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CaelumEstoque.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+
+        [StringLength(20, MinimumLength = 5)]
+        public String Nome { get; set; }
+        [Range(1,999999999999)]
+        public float Preco { get; set; }
+        public CategoriaDoProduto Categoria { get; set; }
+        public int? CategoriaId { get; set; }
+        [StringLength(50, MinimumLength = 5)]
+        public String Descricao { get; set; }
+        [Range(1,99999999999)]
+        public int Quantidade { get; set; }
+    }
+}
